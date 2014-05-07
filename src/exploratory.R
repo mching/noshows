30ec35bf76af9fd51c5ee961cd8ed7da25dc8f93
Michael Ching <mikeching@yahoo.com>
2014-05-06 18:22:54 -1000

    initial

---
+++ src/exploratory.R
@@ -0,0 +1,11 @@
+###########################
+# Exploratory data
+#
+# This file provides steps to clean and recode data and should automatically load
+# if using ProjectTemplate load.project function.
+#
+# Michael Ching, MD, MPH 
+###########################
+
+# Day.of.the.Week.of.Appointment
+table(appts$Day.of.the.Week.of.Appointment)
