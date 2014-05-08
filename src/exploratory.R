9d64e39f9329727b844adfe028585ba84e938446
Michael Ching <mikeching@yahoo.com>
2014-05-07 22:18:05 -1000

    add days of the week, reminder call

--- src/exploratory.R
+++ src/exploratory.R
@@ -7,5 +7,15 @@
 # Michael Ching, MD, MPH 
 ###########################
 
+names(appts)
+
 # Day.of.the.Week.of.Appointment
-table(appts$Day.of.the.Week.of.Appointment)
+plot(appts$ApptWeekday)
+table <- table(appts$ApptWeekday)
+binom.confint(table, n = sum(table), method = "exact")
+
+# Reminder call
+plot(appts$Reminded)
+table <- table(appts$Reminded)
+binom.confint(x = table[2], n = sum(table), method = "exact")
+
